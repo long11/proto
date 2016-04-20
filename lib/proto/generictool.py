aad85d9c7dce86ed1929d4c4c5163a430019864d
Sveinung Gundersen <sveinugu@gmail.com>
2016-04-20 19:35:55 +0200

    Fixed bug in ExploreToolsTool

--- lib/proto/generictool.py
+++ lib/proto/generictool.py
@@ -75,7 +75,7 @@ class GenericToolController(BaseToolController):
         subClasses = self.prototype.getSubToolClasses()
         if subClasses:
             self.subToolSelectionTitle = self.prototype.getSubToolSelectionTitle()
-            self.subClasses[self.prototype.getToolSelectionName()] = self.prototype
+            self.subClasses[self.prototype.getToolSelectionName()] = self.prototype.__class__
             for subcls in subClasses:
                 toolSelectionName = subcls.getToolSelectionName()
                 if self.prototype.useSubToolPrefix():
